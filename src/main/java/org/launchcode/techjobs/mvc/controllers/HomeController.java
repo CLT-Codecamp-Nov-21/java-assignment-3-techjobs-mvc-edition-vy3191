c2ad103a2aa6c4187888739cb59725fad7459cde
vy3191 <venkyafhs@gmail.com>
2022-02-24 21:33:33 -0500

    complete the list views

--- src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
@@ -1,18 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
-
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
-
     @GetMapping(value = "")
     public String index(Model model) {
 
@@ -24,5 +21,4 @@ public class HomeController {
 
         return "index";
     }
-
 }
--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -7,20 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
-
 /**
  * Created by LaunchCode
  */
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
